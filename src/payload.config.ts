d4c295c78dde44b57246da325de12e9fc228f597
johnbalili <31870373+johnbalili@users.noreply.github.com>
2024-08-13 08:29:35 +0800

    added richtext

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,6 +5,9 @@ import { postgresAdapter } from '@payloadcms/db-postgres'
 import { webpackBundler } from '@payloadcms/bundler-webpack'
 import { slateEditor } from '@payloadcms/richtext-slate'
 import { buildConfig } from 'payload/config'
+import {
+  lexicalEditor
+} from '@payloadcms/richtext-lexical'
 
 import Users from './collections/Users'
 
@@ -14,7 +17,95 @@ export default buildConfig({
     bundler: webpackBundler(),
   },
   editor: slateEditor({}),
-  collections: [Users],
+  collections: [
+    {
+      slug: 'posts',
+      fields:[
+        {
+        name: 'title',
+        type: 'text',
+        required: true,
+        },
+        {
+          name: 'slug',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'author',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'position',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'author_image',
+          type: 'upload',
+          relationTo: 'media',
+        },
+        {
+          name: 'category',
+          type: 'relationship',
+          relationTo: 'categories',
+        },
+        {
+          name: 'date',
+          type: 'date',
+        },
+        {
+          name: 'content',
+          type: 'richText',
+          editor: lexicalEditor(),
+        },
+        {
+          name: 'excerpt',
+          type: 'textarea',
+        },
+        {
+          name: 'featured_image',
+          type: 'upload',
+          relationTo: 'media'
+        }
+      ]
+    },
+    { 
+      slug: 'media',
+      fields: [
+        {
+          name: 'alt',
+          type: 'text',
+        }
+      ],
+      upload: true
+    },
+    {
+      slug: 'categories',
+      fields: [
+        {
+          name: 'title',
+          type: 'text',
+        },
+        {
+          name: 'slug',
+          type: 'text',
+        },
+        {
+          name: 'image',
+          type: 'upload',
+          relationTo: 'media',
+        }
+      ],
+    },
+    Users
+  ],
+  upload: {
+    limits: {
+      fileSize: 2000000,
+    }
+  },
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
   },
